ec3446b93d0458f9f254c7164e5e6cc6606a2950
Derek <derek@frisbeeworld.com>
2025-12-29 16:28:42 +1100

    Add application build

--- run.sh
+++ run.sh
@@ -8,9 +8,20 @@ QEMU=qemu-system-riscv32
 CC=/usr/bin/clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
 
+# Path to the llvm objcopy tool
+OBJCOPY=/usr/bin/llvm-objcopy
+
+# Build the user application (shell)
+$CC $CFLAGS -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf \
+	shell.c user.c common.c
+
+# Copy the shell binary to prepare for the kernel build
+$OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
+$OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
+
 # Build the kernel
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-	kernel.c common.c
+	kernel.c common.c shell.bin.o
 
 # Start QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
